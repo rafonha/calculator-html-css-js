287b8a5d93b736dd2116cbe084acba0f5564488b
rafonha <rafonha@hotmail.com>
2023-01-16 16:00:18 -0300

    finish development

--- app.js
+++ app.js
@@ -1,22 +1,43 @@
-let a = document.getElementById("firstNumber").value;
-let b = document.getElementById("secondNumber").value;
 let sumRes = document.getElementById("sum");
-let subtractRes = document.getElementById("minus");
+let subtractARes = document.getElementById("minusA");
+let subtractBRes = document.getElementById("minusB");
 let multiplicationRes = document.getElementById("multiplication");
-let divisionRes = document.getElementById("division");
-let powerRes = document.getElementById("power");
-let squareRes = document.getElementById("squareRoot");
-let percentageRes = document.getElementById("percentage");
+let divisionARes = document.getElementById("divisionA");
+let divisionBRes = document.getElementById("divisionB");
+let powerARes = document.getElementById("powerA");
+let powerBRes = document.getElementById("powerB");
+let squareARes = document.getElementById("squareRootA");
+let squareBRes = document.getElementById("squareRootB");
+let factorialARes = document.getElementById("factorialA");
+let factorialBRes = document.getElementById("factorialB");
+let percentageARes = document.getElementById("percentageA");
+let percentageBRes = document.getElementById("percentageB");
 let averageRes = document.getElementById("average");
+
+sumRes.innerHTML = 0;
+subtractARes.innerHTML = 0;
+subtractBRes.innerHTML = 0;
+multiplicationRes.innerHTML = 0;
+divisionARes.innerHTML = 0;
+divisionBRes.innerHTML = 0;
+powerARes.innerHTML = 0;
+powerBRes.innerHTML = 0;
+squareARes.innerHTML = 0;
+squareBRes.innerHTML = 0;
+factorialARes.innerHTML = 0;
+factorialBRes.innerHTML = 0;
+percentageARes.innerHTML = 0;
+percentageBRes.innerHTML = 0;
+averageRes.innerHTML = 0;
 
 
 const sum = (a, b) => {
     sumRes.innerHTML = a + b;
-    console.log("valor de sumRes", sumRes)  
 };
 
 const subtraction = (a, b) => {
-    subtractRes.innerHTML = a - b;
+    subtractARes.innerHTML = a - b;
+    subtractBRes.innerHTML = b - a;
 };
 
 const multiplication = (a, b) => {
@@ -24,34 +45,71 @@ const multiplication = (a, b) => {
 };
 
 const division = (a, b) => {
-    divisionRes.innerHTML = a / b;
+    divisionARes.innerHTML = (a / b).toFixed(2);
+    divisionBRes.innerHTML = (b / a).toFixed(2);
 };
 
 const power = (a, b) => {
-    powerRes.innerHTML = Math.pow(a, b);
+    powerARes.innerHTML = Math.pow(a, b);
+    powerBRes.innerHTML = Math.pow(b, a);
 };
 
 const squareRoot = (a, b) => {
-    squareRes.innerHTML = Math.sqrt((a * a) + (b * b));
+    squareARes.innerHTML = Math.sqrt(a).toFixed(2);
+    squareBRes.innerHTML = Math.sqrt(b).toFixed(2);
+};
+
+const factorial = (a, b) => {
+    if (a < 19) {
+        let factorialAvalue = 1;
+        
+        for (var i = a; i > 1; i--) {
+            factorialAvalue *= i;
+            console.log('factorialAvalue', factorialAvalue)
+        }
+    
+        factorialARes.innerHTML = factorialAvalue;
+    } else {
+        factorialARes.innerHTML = 'Valor MUITO grande';
+    };
+
+    if (b < 19) {
+        let factorialBvalue = 1;
+    
+        for (var i = b; i > 1; i--) {
+            factorialBvalue *= i;
+        }
+        
+        factorialBRes.innerHTML = factorialBvalue;
+    } else {
+        factorialBRes.innerHTML = 'Valor MUITO grande';
+    };
+
 };
 
 const percentage = (a, b) => {
-    percentageRes.innerHTML = a % b;
+    percentageARes.innerHTML = ((b * 100)/a).toFixed(0) + '%';
+    percentageBRes.innerHTML = ((a * 100)/b).toFixed(0) + '%';
 };
 
 const average = (a, b) => {
     averageRes.innerHTML = (a + b)/2;
 };
 
-const calculate = (a, b) => {
-    preventDefault();
-    console.log("estou em calculate");
+const calculate = () => {
+    let aInput = document.getElementById("firstNumber").value;
+    let bInput = document.getElementById("secondNumber").value;
+
+    let a = parseInt(aInput);
+    let b = parseInt(bInput);
+
     sum(a, b);
     subtraction(a, b);
     multiplication(a, b);
     division(a, b);
     power(a, b);
     squareRoot(a, b);
+    factorial(a, b)
     percentage(a, b);
     average(a, b);
 }
